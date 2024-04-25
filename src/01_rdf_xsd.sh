566601969e0a4392e29775a0fa072a7f1822bb4a
gezever <gezever@skynet.be>
2024-04-25 19:17:58 +0200

    xsd generation

--- src/01_rdf_xsd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-riot --output=rdf/xml ./main/resources/be/vlaanderen/bodemenondergrond/data/id/conceptscheme/bemonsteringstype/bemonsteringstype.ttl > ./main/resources/be/vlaanderen/bodemenondergrond/data/id/conceptscheme/bemonsteringstype/bemonsteringstype.rdf
-
-xsltproc ./source/xsl/rdf_to_xsd.xsl ./main/resources/be/vlaanderen/bodemenondergrond/data/id/conceptscheme/bemonsteringstype/bemonsteringstype.rdf > ./main/resources/be/vlaanderen/bodemenondergrond/data/id/conceptscheme/bemonsteringstype/bemonsteringstype.xsd
